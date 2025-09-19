da78b91c90ba13585a945051d4ccabacabed06e4
s311354 <shelton@>
2025-09-19 01:48:04 +0000

    implement TextQuery pattern; update query; add Query_base Query QueryResult WordQuery NotQuery BinaryQuery AndQuery OrQuery

--- src/query.cc
+++ src/query.cc
@@ -1,40 +1,11 @@
-#include "purgatory.h"
+#include "query.h"
 
 namespace query {
-
-using line_no = vector<string>::size_type;
-class QueryResult;
 
 string make_plural(size_t ctr, const string &word,
 		  const string &ending) {
     return (ctr > 1) ? word + ending : word;
 }
-
-class QueryResult {
-friend ostream& print(ostream&, const QueryResult&);
-
-public:
-    QueryResult(string s, 
-		shared_ptr<set<line_no>> p,
-		shared_ptr<vector<string>> f): sought(s), lines(p), file(f) { }
-
-private:
-    string sought;
-    shared_ptr<set<line_no>> lines;
-    shared_ptr<vector<string>> file;
-};
-
-class TextQuery {
-public:
-
-    TextQuery(ifstream&);
-    QueryResult query(const string&) const;
-
-private:
-    shared_ptr<vector<string>> file;
-    map<string, shared_ptr<set<line_no>>> wm;
-
-};
 
 TextQuery::TextQuery(ifstream &is): file(new vector<string>) {
     string text;
@@ -67,6 +38,58 @@ QueryResult TextQuery::query(const string &sought) const {
     }
 }
 
+inline Query::Query(const string &s): q(new WordQuery(s)) {
+}
+
+inline Query operator~(const Query &operand) {
+    return shared_ptr<Query_base> (new NotQuery(operand));
+}
+inline Query operator&(const Query &lhs, const Query &rhs) {
+    return shared_ptr<Query_base> (new AndQuery(lhs, rhs)); 
+}
+inline Query operator|(const Query &lhs, const Query &rhs) {
+    return shared_ptr<Query_base> (new OrQuery(lhs, rhs));
+}
+
+QueryResult OrQuery::eval(const TextQuery& text) const {
+    auto right = rhs.eval(text), left = lhs.eval(text);
+
+    auto ret_lines = make_shared<set<line_no>>(left.begin(), left.end());
+
+    ret_lines->insert(right.begin(), right.end());
+
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
+
+QueryResult NotQuery::eval(const TextQuery& text) const {
+    auto result = query.eval(text);
+
+    auto ret_lines = make_shared<set<line_no>>();
+
+    auto beg = result.begin(), end = result.end();
+
+    auto sz = result.get_file()->size();
+
+    for (size_t n = 0; n != sz; ++n) {
+        if (beg == end || *beg != n)
+            ret_lines->insert(n);
+	else if (beg != end)
+	    ++beg;
+    }
+
+    return QueryResult(rep(), ret_lines, result.get_file());
+}
+
+QueryResult AndQuery::eval(const TextQuery& text) const {
+    auto left = lhs.eval(text), right = rhs.eval(text);
+
+    auto ret_lines = make_shared<set<line_no>>();
+
+    set_intersection(left.begin(), left.end(), right.begin(), right.end(), inserter(*ret_lines, ret_lines->begin()));
+
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
+
 ostream &print(ostream& os, const QueryResult &qr) {
     os << qr.sought << "occurs " << qr.lines->size() << " "
        << make_plural(qr.lines->size(), "time", "s") << endl;
@@ -77,6 +100,10 @@ ostream &print(ostream& os, const QueryResult &qr) {
     }
 
     return os;
+}
+
+ostream &operator<<(ostream &os, const Query &query) {
+    return os << query.rep();
 }
 
 }
---
+++ src/query.h
@@ -0,0 +1,126 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+#include "purgatory.h"
+
+namespace query {
+
+class QueryResult;
+
+class TextQuery {
+public:
+    using line_no = vector<string>::size_type;
+
+    TextQuery(ifstream&);
+
+    QueryResult query(const string&) const;
+
+private:
+    shared_ptr<vector<string>> file;
+    map<string, shared_ptr<set<line_no>>> wm;
+};
+
+class QueryResult {
+    friend ostream& print(ostream&, const QueryResult&);
+
+public:
+    QueryResult(string s, 
+		shared_ptr<set<TextQuery::line_no>> p,
+		shared_ptr<vector<string>> f): sought(s), lines(p), file(f) { }
+    
+    set<TextQuery::line_no>::iterator begin() { return lines->begin(); }
+
+    set<TextQuery::line_no>::iterator end() { return lines->end(); }
+
+    shared_ptr<vector<string>> get_file() const { return file; }
+
+private:
+    string sought;
+    shared_ptr<set<TextQuery::line_no>> lines;
+    shared_ptr<vector<string>> file;
+};
+
+/*
+ * Its role is to serve as the root of our query hierarchy. It is the abstract base class.
+ */
+class Query_base {
+    friend class Query;
+
+protected:
+    using line_no = TextQuery::line_no;
+
+    virtual ~Query_base() = default;
+
+private:
+    virtual QueryResult eval(const TextQuery&) const = 0;
+    virtual std::string rep() const = 0;
+};
+
+/*
+ *  An interface class hides the hierarchy. It is the handle class that users interact with.
+ */
+class Query {
+    friend Query operator~(const Query &);
+    friend Query operator|(const Query&, const Query&);
+    friend Query operator&(const Query&, const Query&);
+
+public:
+    Query(const string&);
+
+    QueryResult eval(const TextQuery &t) const { return q->eval(t); }
+    string rep() const { return q->rep(); }
+
+private:
+    Query(shared_ptr<Query_base> query): q(query) { }
+    shared_ptr<Query_base> q;
+};
+
+// Derived classes
+class WordQuery: public Query_base {
+    friend class Query;
+
+    WordQuery(const string &s): query_word(s) { }
+
+    QueryResult eval(const TextQuery &t) const { return t.query(query_word); }
+    string rep() const { return query_word; }
+
+    string query_word;
+};
+
+class NotQuery: public Query_base {
+    friend Query operator~(const Query &);
+
+    NotQuery(const Query &q): query(q) { }
+
+    QueryResult eval(const TextQuery&) const;
+    string rep() const { return "~(" + query.rep() + ")"; }
+
+    Query query;
+};
+
+class BinaryQuery: public Query_base {
+protected:
+    BinaryQuery(const Query &l, const Query &r, string s): lhs(l), rhs(r), opSym(s) { }
+    string rep() const { return "(" + lhs.rep() + " " + opSym + " " + rhs.rep() + ")"; }
+    
+    Query lhs, rhs;
+    string opSym;    
+};
+
+class AndQuery: public BinaryQuery {
+    friend Query operator&(const Query&, const Query&);
+    AndQuery(const Query &left, const Query &right): BinaryQuery(left, right, "&") { }
+
+    QueryResult eval(const TextQuery&) const;
+};
+
+class OrQuery: public BinaryQuery {
+    friend Query operator|(const Query&, const Query&);
+    OrQuery(const Query &left, const Query &right): BinaryQuery(left, right, "|") { }
+    
+    QueryResult eval(const TextQuery&) const;
+};
+
+}
+
+#endif // QUERY_H
